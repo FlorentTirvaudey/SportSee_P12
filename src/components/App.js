306ed195947624c031e63c2d892df61fa297f11c
FlorentTirvaudey <florent.tirvaudey@gmail.com>
2024-06-02 00:22:28 +0200

    style: add previsualisation css on homepage

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,41 @@
 import '../styles/App.css';
 
+import Navbar from './Navbar';
+
 function App() {
   return (
     <div>
-      
+      <Navbar />
+      <div className='homepage'>
+        <aside className='sidebar'>
+          
+          <span>Copiryght, SportSee 2020</span>
+        </aside>
+        <section className='main_content'>
+          <div className='content'>
+            <div className='title_part'>
+              <h1>Bonjour</h1>
+              <h2>Félicitation ! Vous avez explosé vos objectifs hier 👏</h2>
+            </div>
+            <div className='stats_part'>
+              <div className='activity'>
+                <div className='chart_1'>Chart 1</div>
+                <div className='details_chart'>
+                  <div className='chart_2'>Chart 2</div>
+                  <div className='chart_3'>Chart 3</div>
+                  <div className='chart_4'>Chart 4</div>
+                </div>
+              </div>
+              <div className='aside_stats'>
+                <div className='test'>Test</div>
+                <div className='test'>Test2</div>
+                <div className='test'>Test</div>
+                
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import '../styles/Navbar.css'
+
+function Navbar() {
+    return (
+        <div className="body_navbar">
+            <div className="logo_navbar">
+                <img src="" alt="logo SportSee" />
+            </div>
+            <div className="content_navbar">
+                <ul>
+                    <li>Accueil</li>
+                    <li>Profil</li>
+                    <li>Réglages</li>
+                    <li>Communauté</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
