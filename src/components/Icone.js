9dc183cf7c4fe079528f085adfb863f3cd293f40
FlorentTirvaudey <florent.tirvaudey@gmail.com>
2024-06-24 10:24:55 +0200

    style: add styles on nav and icons

--- src/components/Icone.js
+++ src/components/Icone.js
@@ -1,11 +1,10 @@
 import '../styles/Icone.css'
 
-import nageur from '../assets/nageurs.png'
-
 function Icone(props) {
+
     return (
         <div className='icone_bloc'>
-            <img src={`src/assets/${props.url}`} alt="icone" />
+            <img src={props.img} alt="icone" />
         </div>
     )
 }
